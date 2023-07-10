703e7ea3ffdf0692802e04367a83b9490bcd0ed6
DONOTINTO <goods99j@gmail.com>
2023-07-10 22:15:37 +0900

    [Update] firebase 이메일 로그인 / 구글 로그인

--- AirPlan/AirPlan/Controller/LoginViewController.swift
+++ AirPlan/AirPlan/Controller/LoginViewController.swift
@@ -6,13 +6,92 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
     let loginView = LoginView()
+    let actionCodeSettings = ActionCodeSettings()
+    
+    override func loadView() {
+        self.view = loginView
+        loginView.initialSetup()
+        loginView.makeUI()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loginView.initialSetup()
-        loginView.makeUI()
+        loginView.loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
+        loginView.signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+        loginView.signInGoogleButton.addTarget(self, action: #selector(signInGoogleButtonClicked), for: .touchUpInside)
+        loginView.testLogOutButton.addTarget(self, action: #selector(testLogOutButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc func loginButtonClicked() {
+        let email = self.loginView.idTextField.text!
+        let password = self.loginView.passwordTextField.text!
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+            guard error == nil else {
+                print(error!.localizedDescription)
+                print("로그인 실패")
+                return
+            }
+            print("로그인 성공")
+        }
+    }
+    
+    @objc func signUpButtonClicked() {
+        let email = self.loginView.idTextField.text!
+        let password = self.loginView.passwordTextField.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            guard error == nil else {
+                print(error!.localizedDescription)
+                print("아이디 생성 실패")
+                return
+            }
+            print("아이디 생성 완료")
+        }
+    }
+    
+    @objc func signInGoogleButtonClicked() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                print("error")
+                return
+            }
+            
+            guard let user = result?.user, let idToken = user.idToken?.tokenString else {
+                print("저기")
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { result, error in
+                print("성공")
+                self.loginView.testLabel.text = result?.user.email
+                // At this point, our user is signed in
+            }
+        }
+    }
+    
+    @objc func testLogOutButtonClicked() {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            self.loginView.testLabel.text = "로그아웃!"
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
     }
 }
--- AirPlan/AirPlan/View/LoginView.swift
+++ AirPlan/AirPlan/View/LoginView.swift
@@ -6,11 +6,86 @@
 //
 
 import UIKit
+import SnapKit
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 
 class LoginView: UIView, UIEssentials {
+    let idTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginButton = UIButton()
+    let signUpButton = UIButton()
+    var signInGoogleButton = GIDSignInButton()
+    let testLabel = UILabel()
+    let testLogOutButton = UIButton()
+    
     func initialSetup() {
+        [idTextField, passwordTextField, loginButton, signUpButton, signInGoogleButton, testLabel, testLogOutButton].forEach { self.addSubview($0)}
+        idTextField.backgroundColor = .lightGray
+        passwordTextField.backgroundColor = .lightGray
+        loginButton.backgroundColor = .systemBlue
+        signUpButton.backgroundColor = .systemGreen
+        testLogOutButton.backgroundColor = .systemGreen
+        loginButton.titleLabel?.textColor = .black
+        signUpButton.titleLabel?.textColor = .black
+        testLogOutButton.titleLabel?.textColor = .black
+        testLabel.text = "123"
+        testLabel.textColor = .black
+        testLabel.numberOfLines = 2
+        
+        idTextField.placeholder = "아이디를 입력해주세요."
+        passwordTextField.placeholder = "비밀번호를 입력해주세요"
+        loginButton.setTitle("로그인", for: .normal)
+        signUpButton.setTitle("회원가입", for: .normal)
+        testLogOutButton.setTitle("로그아웃", for: .normal)
     }
     
     func makeUI() {
+        idTextField.snp.makeConstraints {
+            $0.height.equalTo(30)
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(200)
+        }
+        
+        passwordTextField.snp.makeConstraints {
+            $0.height.equalTo(30)
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(idTextField.snp.bottom).offset(10)
+        }
+        
+        loginButton.snp.makeConstraints {
+            $0.height.equalTo(30)
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(10)
+        }
+        
+        signUpButton.snp.makeConstraints {
+            $0.height.equalTo(30)
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(loginButton.snp.bottom).offset(10)
+        }
+        
+        signInGoogleButton.snp.makeConstraints {
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(signUpButton.snp.bottom).offset(10)
+        }
+        
+        testLabel.snp.makeConstraints{
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(signInGoogleButton.snp.bottom).offset(10)
+        }
+        
+        testLogOutButton.snp.makeConstraints {
+            $0.leading.equalTo(self.snp.leading).offset(10)
+            $0.trailing.equalTo(self.snp.trailing).offset(-10)
+            $0.top.equalTo(testLabel.snp.bottom).offset(10)
+        }
     }
 }
