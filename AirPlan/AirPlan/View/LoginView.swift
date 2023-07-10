4b7407d25f9f75cb88300085d2f64cbc541f56f5
DONOTINTO <goods99j@gmail.com>
2023-07-10 19:46:37 +0900

    [Add] LoginView File

---
+++ AirPlan/AirPlan/View/LoginView.swift
@@ -0,0 +1,16 @@
+//
+//  LoginView.swift
+//  AirPlan
+//
+//  Created by 이중엽 on 2023/07/10.
+//
+
+import UIKit
+
+class LoginView: UIView, UIEssentials {
+    func initialSetup() {
+    }
+    
+    func makeUI() {
+    }
+}
