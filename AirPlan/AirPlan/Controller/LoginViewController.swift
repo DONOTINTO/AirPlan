b3c80698c830651f9cd13d3cddbc1056642fb1c9
DONOTINTO <goods99j@gmail.com>
2023-07-12 22:42:54 +0900

    [Update] 주석 및 코드 정리

--- AirPlan/AirPlan/Controller/LoginViewController.swift
+++ AirPlan/AirPlan/Controller/LoginViewController.swift
@@ -29,6 +29,7 @@ class LoginViewController: UIViewController {
         loginView.testLogOutButton.addTarget(self, action: #selector(testLogOutButtonClicked), for: .touchUpInside)
     }
     
+    //일반 이메일 - 로그인 버튼 클릭 시
     @objc func loginButtonClicked() {
         let email = self.loginView.idTextField.text!
         let password = self.loginView.passwordTextField.text!
@@ -36,14 +37,13 @@ class LoginViewController: UIViewController {
             guard let strongSelf = self else { return }
             guard error == nil else {
                 print(error!.localizedDescription)
-                print("로그인 실패")
                 return
             }
-            print("로그인 성공")
             self?.loginView.makeLogout(status: .login)
         }
     }
     
+    //일반 이메일 - 회원가입 버튼 클릭 시
     @objc func signUpButtonClicked() {
         let email = self.loginView.idTextField.text!
         let password = self.loginView.passwordTextField.text!
@@ -51,13 +51,13 @@ class LoginViewController: UIViewController {
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             guard error == nil else {
                 print(error!.localizedDescription)
-                print("아이디 생성 실패")
                 return
             }
-            print("아이디 생성 완료")
+            print("SignUp Success")
         }
     }
     
+    //구글 - 구글 로그인/회원가입 버튼 클릭 시
     @objc func signInGoogleButtonClicked() {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
         
@@ -66,24 +66,23 @@ class LoginViewController: UIViewController {
         
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
             guard error == nil else {
-                print("error")
+                print(error.debugDescription)
                 return
             }
             
             guard let user = result?.user, let idToken = user.idToken?.tokenString else {
-                print("저기")
                 return
             }
             
             let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
             Auth.auth().signIn(with: credential) { result, error in
-                print("성공")
                 self.loginView.testLabel.text = result?.user.email
                 self.loginView.makeLogout(status: .login)
             }
         }
     }
     
+    //로그아웃
     @objc func testLogOutButtonClicked() {
         let firebaseAuth = Auth.auth()
         do {
