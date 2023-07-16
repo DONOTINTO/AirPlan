61857709d9e1c804378cf7e1504fd2e2921defb9
DONOTINTO <goods99j@gmail.com>
2023-07-16 19:02:16 +0900

    [Update] 금일 날짜 표기

---
+++ AirPlan/AirPlan/Model/IQAirDate.swift
@@ -0,0 +1,28 @@
+//
+//  IQAirDate.swift
+//  AirPlan
+//
+//  Created by 이중엽 on 2023/07/16.
+//
+
+import Foundation
+
+class IQAirDate {
+    static var shared = IQAirDate()
+    
+    private let date = Date()
+    private let dateFomatter = DateFormatter()
+    
+    private init() { }
+    
+    private func setTimeFormatter() {
+        IQAirDate.shared.dateFomatter.timeStyle = .none
+        IQAirDate.shared.dateFomatter.locale = Locale(identifier: "ko-KR")
+        IQAirDate.shared.dateFomatter.dateFormat = "오늘\n MMM d일 (E)"
+    }
+    
+    func fetchDate() -> String {
+        setTimeFormatter()
+        return IQAirDate.shared.dateFomatter.string(from: date)
+    }
+}
