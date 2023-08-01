d04d03e7050050a981a39b7755881bd64e3cb175
DONOTINTO <goods99j@gmail.com>
2023-08-01 22:46:00 +0900

    [Add] SignUpView 추가

---
+++ AirPlan/AirPlan/View/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  AirPlan
+//
+//  Created by 이중엽 on 2023/08/01.
+//
+
+import UIKit
+
+class SignUpView: UIView, UIEssentials {
+    func initialSetup() {
+        
+    }
+    
+    func makeUI() {
+        
+    }
+}
