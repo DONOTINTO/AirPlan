b78796999c69664541013445c7caaa9ce006d99b
DONOTINTO <goods99j@gmail.com>
2023-08-07 23:08:48 +0900

    [Update] 데이터 호출 1

--- AirPlan/AirPlan/Controller/SignUpViewController.swift
+++ AirPlan/AirPlan/Controller/SignUpViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class SignUpViewController: UIViewController {
     let signUpView = SignUpView()
@@ -25,18 +26,31 @@ class SignUpViewController: UIViewController {
         signUpView.makeUI()
         
         signUpView.signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+        signUpView.idDuplicateCheckButton.addTarget(self, action: #selector(DuplicateCheckButtonClicked), for: .touchUpInside)
     }
     
     @objc func signUpButtonClicked() {
         let email = self.signUpView.idTextField.text!
         let password = self.signUpView.passwordTextField.text!
+        let nickname = self.signUpView.nicknameTextField.text!
         
+        // FireStoreData.shared.UserData(id: email, password: password, nickname: nickname)
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             guard error == nil else {
                 print(error!.localizedDescription)
                 return
             }
             print("SignUp Success")
+        }
+    }
+    
+    @objc func DuplicateCheckButtonClicked() {
+        let email = self.signUpView.idTextField.text!
+        let userData = FireStoreData.shared.UserData()
+        
+        let query = userData.whereField("ID", isEqualTo: email)
+        query.getDocuments { qs, error in
+            print(qs?.documents)
         }
     }
 }
---
+++ AirPlan/AirPlan/Model/FireStoreData.swift
@@ -0,0 +1,20 @@
+//
+//  FireStoreData.swift
+//  AirPlan
+//
+//  Created by 이중엽 on 2023/08/06.
+//
+
+import UIKit
+import FirebaseFirestore
+
+class FireStoreData {
+    static let shared = FireStoreData()
+    
+    private init() {}
+    
+    func UserData() -> CollectionReference {
+        let db = Firestore.firestore()
+        return db.collection("Users")
+    }
+}
