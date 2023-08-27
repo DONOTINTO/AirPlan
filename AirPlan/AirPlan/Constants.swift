d282ce97b76ffca208851e58939aee09dbaee63e
DONOTINTO <goods99j@gmail.com>
2023-08-27 22:18:35 +0900

    [Add] Constants 추가

---
+++ AirPlan/AirPlan/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  AirPlan
+//
+//  Created by 이중엽 on 2023/08/27.
+//
+
+import UIKit
+
+class Constants {
+    static let WRONGEMAIL = "이메일을 정확히 입력해주세요."
+    static let EMPTYEMAIL = "이메일을 입력해주세요."
+    static let DUPLICATEEMAIL = "생성된 이메일이 존재합니다"
+    static let WRONGNICKNAME = "한글,영문(숫자포함) 2~8자까지 사용 가능합니다"
+    static let EMPTYNICKNAME = "닉네임을 입력해주세요"
+    static let DUPLICATENICKNAME = "중복된 닉네임입니다"
+    static let CONFIRMPASSWORD = "비밀번호가 일치하지 않습니다"
+}
+
