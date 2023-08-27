fc1585ab72028bfb49c7c11b592b5549186e2609
DONOTINTO <goods99j@gmail.com>
2023-08-27 22:18:58 +0900

    [Update] 로그인 에러 표시 구현

--- AirPlan/AirPlan/Controller/SignUpViewController.swift
+++ AirPlan/AirPlan/Controller/SignUpViewController.swift
@@ -9,13 +9,8 @@ import UIKit
 import FirebaseAuth
 import FirebaseFirestore
 
-let DUPLICATEID = "duplicateID"
-let CONFIRMPASSWORD = "confirmPassword"
-let DUPLICATENICKNAME = "duplicateNickname"
-
 class SignUpViewController: UIViewController {
     let signUpView = SignUpView()
-    var checkList = [DUPLICATEID: false, CONFIRMPASSWORD: false, DUPLICATENICKNAME: false]
     
     override func loadView() {
         self.view = signUpView
@@ -62,22 +57,18 @@ class SignUpViewController: UIViewController {
     }
     
     @objc func idDuplicateCheckButtonClicked(_ sender: UIButton) {
-        // 1. 정규식대로 썼는가
-            // 1-1. 정규식 대로 썻을 때
-            // 2번으로 넘어감
-            // 1-2. 정규식 대로 안썻을 때
-            // 안내 문구(이메일을 정확히 입력해주세요)
-        // 2. 중복값이 있는가
-            // 2-1. 중복값이 없을 때
-            // 체크 버튼이 비활성화, 체크 확인
-            // 2-2. 중복값이 있을 때
-            // 안내 문구(생성된 아이디가 존재합니다.)
-        
         let email = self.signUpView.idTextField.text!
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
         
+        self.signUpView.errorMSG(msg: "")
+        
+        if email == "" {
+            self.signUpView.errorMSG(msg: Constants.EMPTYEMAIL)
+            return
+        }
+        
         if NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email) != true {
-            print("이메일을 정확히 입력해주세요")
+            self.signUpView.errorMSG(msg: Constants.WRONGEMAIL)
             return
         }
         
@@ -86,12 +77,12 @@ class SignUpViewController: UIViewController {
         query.getDocuments { document, error in
             if document!.documents.isEmpty {
                 //중복 체크 통과
-                self.checkList.updateValue(true, forKey: DUPLICATEID)
-                self.signUpView.nicknameDuplicateCheckButton.backgroundColor = .lightGray
+                self.signUpView.idDuplicateCheckButton.backgroundColor = .lightGray
+                self.signUpView.errorMSG(msg: "")
             } else {
                 //중복 체크 미통과
-                self.signUpView.nicknameDuplicateCheckButton.backgroundColor = UIColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1)
-                print("생성된 이메일이 존재합니다")
+                self.signUpView.idDuplicateCheckButton.backgroundColor = UIColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1)
+                self.signUpView.errorMSG(msg: Constants.DUPLICATEEMAIL)
             }
         }
     }
@@ -100,13 +91,15 @@ class SignUpViewController: UIViewController {
         let nickname = self.signUpView.nicknameTextField.text!
         let nicknameRegex = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,8}$"
         
+        self.signUpView.errorMSG(msg: "")
+        
         if nickname == "" {
-            print("닉네임을 입력해주세요.")
+            self.signUpView.errorMSG(msg: Constants.EMPTYNICKNAME)
             return
         }
         
         if NSPredicate(format: "SELF MATCHES %@", nicknameRegex).evaluate(with: nickname) != true {
-            print("띄어쓰기 없이 한글,영문(숫자포함) 2~8자까지 사용 가능합니다 ")
+            self.signUpView.errorMSG(msg: Constants.WRONGNICKNAME)
             return
         }
         
@@ -115,23 +108,22 @@ class SignUpViewController: UIViewController {
         query.getDocuments { document, error in
             if document!.documents.isEmpty {
                 //중복 체크 통과
-                self.checkList.updateValue(true, forKey: DUPLICATENICKNAME)
                 self.signUpView.nicknameDuplicateCheckButton.backgroundColor = .lightGray
+                self.signUpView.errorMSG(msg: "")
             } else {
                 //중복 체크 미통과
                 self.signUpView.nicknameDuplicateCheckButton.backgroundColor = UIColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1)
-                print("중복된 닉네임입니다")
+                self.signUpView.errorMSG(msg: Constants.DUPLICATENICKNAME)
             }
         }
     }
     
     @objc func didIDTextFieldChange() {
         self.signUpView.idDuplicateCheckButton.backgroundColor = UIColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1)
-        checkList.updateValue(false, forKey: DUPLICATEID)
+        
     }
     
     @objc func didNicknameTextFieldChange() {
         self.signUpView.nicknameDuplicateCheckButton.backgroundColor = UIColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1)
-        checkList.updateValue(false, forKey: DUPLICATENICKNAME)
     }
 }
--- AirPlan/AirPlan/View/SignUpView.swift
+++ AirPlan/AirPlan/View/SignUpView.swift
@@ -12,16 +12,23 @@ class SignUpView: UIView, UIEssentials {
     let logoImageView = UIImageView()
     let infoLabel = UILabel()
     let idTextField = UITextField()
+    let idErrorLabel = UILabel()
     let idDuplicateCheckButton = UIButton()
     let passwordTextField = UITextField()
     let passwordCheckTextField = UITextField()
+    let passwordErrorLabel = UILabel()
     let nicknameLabel = UILabel()
     let nicknameTextField = UITextField()
+    let nicknameErrorLabel = UILabel()
     let nicknameDuplicateCheckButton = UIButton()
     let signUpButton = UIButton()
     
     func initialSetup() {
         [logoImageView, infoLabel, idTextField, idDuplicateCheckButton, passwordTextField, passwordCheckTextField, nicknameLabel, nicknameTextField, nicknameDuplicateCheckButton, signUpButton].forEach {
+            self.addSubview($0)
+        }
+        
+        [idErrorLabel, passwordErrorLabel, nicknameErrorLabel].forEach {
             self.addSubview($0)
         }
         
@@ -36,6 +43,9 @@ class SignUpView: UIView, UIEssentials {
         idTextField.layer.borderColor = CGColor(red: 216/255, green: 219/255, blue: 227/255, alpha: 1)
         idTextField.layer.borderWidth = 1
         idTextField.layer.cornerRadius = 5
+        
+        idErrorLabel.font = .boldSystemFont(ofSize: 12)
+        idErrorLabel.textColor = .lightGray
         
         idDuplicateCheckButton.backgroundColor = UIColor(cgColor: CGColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1))
         idDuplicateCheckButton.setTitle("중복확인", for: .normal)
@@ -56,6 +66,9 @@ class SignUpView: UIView, UIEssentials {
         passwordCheckTextField.layer.cornerRadius = 5
         passwordCheckTextField.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         
+        passwordErrorLabel.font = .boldSystemFont(ofSize: 12)
+        passwordErrorLabel.textColor = .lightGray
+        
         nicknameLabel.text = "닉네임"
         
         nicknameTextField.placeholder = "닉네임을 입력하세요."
@@ -63,6 +76,9 @@ class SignUpView: UIView, UIEssentials {
         nicknameTextField.layer.borderColor = CGColor(red: 216/255, green: 219/255, blue: 227/255, alpha: 1)
         nicknameTextField.layer.borderWidth = 1
         nicknameTextField.layer.cornerRadius = 3
+        
+        nicknameErrorLabel.font = .boldSystemFont(ofSize: 12)
+        nicknameErrorLabel.textColor = .lightGray
         
         nicknameDuplicateCheckButton.backgroundColor = UIColor(cgColor: CGColor(red: 0/255, green: 168/255, blue: 168/255, alpha: 1))
         nicknameDuplicateCheckButton.setTitle("중복확인", for: .normal)
@@ -99,19 +115,19 @@ class SignUpView: UIView, UIEssentials {
             $0.height.equalTo(50)
             $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
             $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
-            $0.top.equalTo(idTextField.safeAreaLayoutGuide.snp.bottom).offset(10)
+            $0.top.equalTo(idTextField.snp.bottom).offset(10)
         }
         
         passwordCheckTextField.snp.makeConstraints {
             $0.height.equalTo(50)
             $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
             $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
-            $0.top.equalTo(passwordTextField.safeAreaLayoutGuide.snp.bottom)
+            $0.top.equalTo(passwordTextField.snp.bottom)
         }
         
         nicknameLabel.snp.makeConstraints {
             $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
-            $0.top.equalTo(passwordCheckTextField.safeAreaLayoutGuide.snp.bottom).offset(30)
+            $0.top.equalTo(passwordCheckTextField.snp.bottom).offset(30)
         }
         
         nicknameTextField.snp.makeConstraints {
@@ -133,6 +149,84 @@ class SignUpView: UIView, UIEssentials {
             $0.top.equalTo(nicknameDuplicateCheckButton.snp.bottom).offset(30)
             $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
             $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
+        }
+    }
+    
+    func errorMSG(msg: String) {
+        switch msg {
+        case Constants.WRONGEMAIL, Constants.DUPLICATEEMAIL, Constants.EMPTYEMAIL:
+            self.addSubview(idErrorLabel)
+            idErrorLabel.text = msg
+            
+            idErrorLabel.snp.makeConstraints {
+                $0.top.equalTo(idTextField.snp.bottom)
+                $0.trailing.equalTo(idTextField.snp.trailing)
+            }
+            
+            passwordTextField.snp.remakeConstraints {
+                $0.height.equalTo(50)
+                $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+                $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
+                $0.top.equalTo(idErrorLabel.snp.bottom).offset(10)
+            }
+            break
+        case Constants.CONFIRMPASSWORD:
+            self.addSubview(passwordErrorLabel)
+            passwordErrorLabel.text = msg
+            
+            passwordErrorLabel.snp.makeConstraints {
+                $0.top.equalTo(passwordCheckTextField.snp.bottom)
+                $0.trailing.equalTo(passwordCheckTextField.snp.trailing)
+            }
+            
+            nicknameLabel.snp.remakeConstraints {
+                $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+                $0.top.equalTo(passwordErrorLabel.snp.bottom).offset(30)
+            }
+            break
+            
+        case Constants.DUPLICATENICKNAME, Constants.EMPTYNICKNAME, Constants.WRONGNICKNAME:
+            self.addSubview(nicknameErrorLabel)
+            nicknameErrorLabel.text = msg
+            
+            nicknameErrorLabel.snp.makeConstraints {
+                $0.top.equalTo(nicknameTextField.snp.bottom)
+                $0.trailing.equalTo(nicknameTextField.snp.trailing)
+            }
+            
+            signUpButton.snp.remakeConstraints {
+                $0.height.equalTo(50)
+                $0.top.equalTo(nicknameErrorLabel.snp.bottom).offset(30)
+                $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+                $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
+            }
+            break
+        default:
+            idErrorLabel.snp.removeConstraints()
+            passwordErrorLabel.snp.removeConstraints()
+            nicknameErrorLabel.snp.removeConstraints()
+            self.idErrorLabel.removeFromSuperview()
+            self.passwordErrorLabel.removeFromSuperview()
+            self.nicknameErrorLabel.removeFromSuperview()
+            
+            passwordTextField.snp.remakeConstraints {
+                $0.height.equalTo(50)
+                $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+                $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
+                $0.top.equalTo(idTextField.snp.bottom).offset(10)
+            }
+            
+            nicknameLabel.snp.remakeConstraints {
+                $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+                $0.top.equalTo(passwordCheckTextField.snp.bottom).offset(30)
+            }
+            
+            signUpButton.snp.remakeConstraints {
+                $0.height.equalTo(50)
+                $0.top.equalTo(nicknameDuplicateCheckButton.snp.bottom).offset(30)
+                $0.leading.equalTo(self.safeAreaLayoutGuide.snp.leading).offset(30)
+                $0.trailing.equalTo(self.safeAreaLayoutGuide.snp.trailing).offset(-30)
+            }
         }
     }
 }
